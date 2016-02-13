10b63698dc92cf63087404d9719ddc2751eb9561
Alex Carstensen <matthacker.alex@gmail.com>
2016-02-13 22:55:49 +0100

    Secret Diary works

--- My Websites/8. MySQL/SecretDiary/SecretDiary/SecretDiary/index.php
+++ My Websites/8. MySQL/SecretDiary/SecretDiary/SecretDiary/index.php
@@ -1,5 +1,196 @@
 <?php
+include("login.php");
 
-echo "Hello World!";
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Secret Diary</title>
+
+    <!-- Bootstrap -->
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        #topContainer {
+            background-image: url("Images/mountain.jpg");
+            height: 400px;
+            width: 100%;
+            background-size: cover;
+        }
+
+        #topRow {
+            margin-top: 100px;
+            text-align: center;
+        }
+
+            #topRow h1 {
+                font-size: 300%;
+            }
+
+        .bold {
+            font-weight: bold;
+        }
+
+        .marginTop {
+            margin-top: 30px;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .title {
+            margin-top: 100px;
+            font-size: 300%;
+        }
+
+        #footer {
+            background-color: lightblue;
+            width: 100%;
+            height: 100px;
+        }
+
+        .marginBottom {
+            margin-bottom: 30px;
+        }
+
+        #appstore {
+            width: 10%;
+            height: 10%;
+
+        }
+    </style>
+
+
+</head>
+<body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" id="navbartop">
+
+        <div class="container">
+
+            <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+
+                </button>
+                <a class="navbar-brand">Secret Diary</a>
+            </div>
+
+            <div class="collapse navbar-collapse">
+
+
+
+                <form class="navbar-form navbar-right" method="post">
+
+                    <div class="form-group">
+
+                        <input type="email" name="loginemail" placeholder="Email" class="form-control" value="<?php echo addslashes($_POST['loginemail']); ?>" />
+
+
+                    </div>
+
+                    <div class="form-group">
+
+                        <input type="password" name="loginpassword" placeholder="Password" class="form-control" value="<?php echo addslashes($_POST['loginpassword']); ?>" />
+
+                    </div>
+
+                    <input type="submit" name="submit" class="btn btn-success" value="Log In" />
+                    
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="container contentContainer" id="topContainer">
+
+        <div class="row">
+
+            <div class="col-md-6 col-md-offset-3" id="topRow">
+
+                <h1 class="marginTop">Secret Diary</h1>
+                <p class="lead">Your own private diary, with you wherever you go.</p>
+
+
+                <?php
+
+                    if($error)
+                    {
+                        echo '<div class="alert alert-danger">' . addslashes($error) . '</div>';
+
+                    }
+                    if($message)
+                    {
+                        echo '<div class="alert alert-success">' . addslashes($message) . '</div>';
+                    }
+
+
+                ?>
+
+                <p class="bold marginTop">Interested? Sign up Below!</p>
+
+                <form class="marginTop" method="post">
+
+                    <div class="form-group">
+                        <label for="email">Email Address</label>
+
+                        <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo addslashes($_POST['email']); ?>" />
+
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+
+                        <input type="password" name="password" class="form-control" placeholder="Password" value="<?php echo addslashes($_POST['password']); ?>" />
+
+
+                    </div>
+
+                    <input type="submit" value="Sign Up" name="submit" class="btn btn-success btn-lg marginTop" />
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="Scripts/bootstrap.min.js"></script>
+
+    <script>
+
+
+            $(".contentContainer").css("min-height", $(window).height());
+
+    </script>
+
+</body>
+</html>
+
+
+
---
+++ My Websites/8. MySQL/SecretDiary/SecretDiary/SecretDiary/login.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+
+if($_GET["logout"]==1)
+{
+    session_destroy();
+
+    $message = "You have been logged out. Have a nice day!";
+
+}
+    
+include("connection.php");
+
+
+
+
+if($_POST['submit']=="Sign Up")
+{
+    $message = "";
+    if(!$_POST['email'])
+        $error.="<br />Please enter an email";
+    else if (!(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)))
+        $error.="<br />The input is not a valid email";
+
+    if(!$_POST['password'])
+        $error.="<br />Please enter a password";
+    else
+    {
+        if(strlen($_POST['password'])<8)
+            $error.="<br />Please enter a password with more than 8 characters.";
+
+        if(!preg_match('/[A-Z]/', $_POST['password']))
+            $error.="<br />Please use atleast one capital letter in your password.";
+    }
+
+    if($error)
+        $error =  "There were error(s) in your signup details: ".$error;
+    
+    else
+    {
+
+
+        $query = "SELECT * FROM `users` Where email = '".mysqli_real_escape_string($link, $_POST['email'])."'";
+
+        $result = mysqli_query($link, $query);
+
+        $results = mysqli_num_rows($result);
+
+
+        if($results)
+            $error = "That email address is already registered. Do you want to log in?";
+        else
+        {
+            $query = "INSERT INTO `users` (`email`, `password`) VALUES('".mysqli_real_escape_string($link, $_POST['email'])."','".md5(md5($_POST['email']).$_POST['password'])."')";
+
+            mysqli_query($link, $query);
+            $error =  "You've been signed up!";
+
+            $_SESSION['id']=mysqli_insert_id($link);
+
+
+            header("Location:mainpage.php");
+        }
+    }
+
+}
+if($_POST['submit']=="Log In")
+{
+    $message = "";
+    $query = "SELECT * FROM users WHERE email='".mysqli_real_escape_string($link, $_POST['loginemail'])."'AND password='" .md5(md5($_POST['loginemail']).$_POST['loginpassword'])."' LIMIT 1";
+
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+
+
+    if($row)
+    {
+        $_SESSION['id']=$row['id'];
+        header("Location:mainpage.php");
+    }
+    else
+    {
+        $error = "We could not find a user with that email and password. Please try again.";
+    }
+}
+
+?>
